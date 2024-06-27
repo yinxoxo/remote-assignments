a8950b5bfac680be4202f3492e1cb0a48312c203
chris <pupu86019@gmail.com>
2024-06-27 22:07:44 +0800

    i is changed to start from 1

--- Week2/Assignment1.js
+++ Week2/Assignment1.js
@@ -1,6 +1,6 @@
 function max(numbers) {
   let maxNumber = numbers[0];
-  for (let i = 0; i < numbers.length; i++) {
+  for (let i = 1; i < numbers.length; i++) {
     if (numbers[i] > maxNumber) {
       maxNumber = numbers[i];
     }
