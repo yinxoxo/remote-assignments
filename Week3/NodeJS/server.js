1a73b2474563a9bcb23d3691af142b6a942ef123
chris <pupu86019@gmail.com>
2024-07-05 15:41:39 +0800

    Complete Assignment1-3

---
+++ Week3/NodeJS/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello, My Server!</h1>");
+});
+
+//Assignment 2
+app.get("/getData", (req, res) => {
+  const number = req.query.number;
+
+  if (number === undefined) {
+    res.send("Lack of Parameter");
+  } else if (
+    //不是數字、整數、正數
+    !Number.isInteger(Number(number) || number <= 0 || isNaN(number))
+  ) {
+    res.send("Wrong Parameter");
+  } else {
+    const num = parseInt(number);
+    let sum = 0;
+    for (let i = 1; i <= num; i++) {
+      sum += i;
+    }
+    res.send(`Sum is ${sum}`);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
+
+//Assignment 3
+app.use(express.static("public"));
