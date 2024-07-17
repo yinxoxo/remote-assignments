04aa8fdf87643ac97ea32506d2361657b556baca
chris <pupu86019@gmail.com>
2024-07-17 23:02:54 +0800

    Modified package-lock.json and package.json to include necessary dependencies

--- Week4/combined-assignment/vite.config.js
+++ Week4/combined-assignment/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/remote-assignments/",
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
